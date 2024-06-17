c404f12086b4fae8b368ce211974afd546106977
eroha <oyeroshkin30@gmail.com>
2024-06-17 12:33:49 +0300

    Initial Commit

---
+++ iOSDevTestApp/iOSDevTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSDevTestAppApp.swift
+//  iOSDevTestApp
+//
+//  Created by oleh yeroshkin on 17.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct iOSDevTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
